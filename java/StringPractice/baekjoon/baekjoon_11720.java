38e6beeffb149f0d89f0f9b7a067c0562433a8f5
sywtit <youworthit17@naver.com>
2021-07-01 21:37:12 +0900

    java-practice : spliting string

---
+++ java/StringPractice/baekjoon/baekjoon_11720.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class baekjoon_11720 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N;
+        N = Integer.parseInt(sc.nextLine());
+        String input;
+
+        int answer = 0;
+        input = sc.nextLine();
+
+        //split 도 하나의 방법일 수 도 있지만
+        /*
+            string.charAt(i) - '0';
+            or
+            Integer.parseInt(inputs.substring(i,i+1));
+        */
+        String[] input_integer = input.split("");
+
+        for (int i = 0; i < N; i++) {
+            answer += Integer.parseInt(input_integer[i]);
+        }
+
+
+        System.out.println(answer);
+        sc.close();
+
+    }
+}
