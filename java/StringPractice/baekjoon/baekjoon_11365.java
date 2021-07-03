f4d23eeecf3963b069c31a4d976ecbd04144fd14
sywtit <youworthit17@naver.com>
2021-07-04 05:36:48 +0900

    java practice : with string and two pointers altorithm

---
+++ java/StringPractice/baekjoon/baekjoon_11365.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class baekjoon_11365 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input, answer;
+
+        while (true) {
+            input = sc.nextLine();
+            // 기저 사례
+            if (input.compareTo("END") == 0) {
+                break;
+            }
+
+            answer = "";
+            for (int i = input.length() - 1; i >= 0; i--) {
+                answer = answer + input.charAt(i);
+            }
+
+            System.out.println(answer);
+
+        }
+        sc.close();
+
+    }
+}
---
+++ java/TwoPointersPractice/baekjoon/baekjoon_11728.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class baekjoon_11728 {
+    public static void main(String[] args) {
+        int N, M;
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        // 개행 문자
+        sc.nextLine();
+
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[M];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        // 개행 문자
+        sc.nextLine();
+        for (int i = 0; i < M; i++) {
+            B[i] = sc.nextInt();
+        }
+
+        // 개행 문자
+        sc.nextLine();
+
+        int A_point = 0, B_point = 0;
+
+        while (A_point != N && B_point != M) {
+
+            if (A[A_point] < B[B_point]) {
+                System.out.print(A[A_point] + " ");
+                A_point++;
+            } else if (A[A_point] > B[B_point]) {
+                System.out.print(B[B_point] + " ");
+                B_point++;
+            } else {
+                System.out.print(A[A_point] + " " + B[B_point] + " ");
+                A_point++;
+                B_point++;
+            }
+
+        }
+
+        if (A_point == N && B_point == M) {
+            // do nothing
+        } else {
+            if (A_point == N) {
+                while (B_point != M) {
+                    System.out.print(B[B_point] + " ");
+                    B_point++;
+                }
+            } else {
+                while (A_point != N) {
+                    System.out.print(A[A_point] + " ");
+                    A_point++;
+                }
+            }
+        }
+
+    }
+}
