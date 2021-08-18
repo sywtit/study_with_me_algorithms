d926a04c93449c5ea779f62d806f79601dfa294f
sooyoung <youworthit17@naver.com>
2021-08-18 19:39:51 +0900

    java:basices:baekjoon:1918:gold3 - have to try 1/2

--- java/Stack/baekjoon/baekjoon_1918.java
+++ java/Stack/baekjoon/baekjoon_1918.java
@@ -4,6 +4,16 @@ import java.util.*;
 import java.io.*;
 
 public class baekjoon_1918 {
+
+    static int precedence(char ch) {
+        if (ch == '(')
+            return 0;
+        else if (ch == '+' || ch == '-')
+            return 1;
+        else
+            return 2;
+    }
+
     public static void main(String[] args) throws Exception {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -12,50 +22,33 @@ public class baekjoon_1918 {
         Deque<Character> s = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
 
-        s.push('(');
-        boolean check = false;
-        Deque<Character> answer = new ArrayDeque<>();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
 
-            if (c != ')') {
-                // peek해서 *가 나오면 처리하기
-                if (c != '(' && !s.isEmpty() && s.peek() == '*') {
-                    add_to_value(s, answer);
-                }
-                s.push(c);
-                if (check == true) {
-                    while (!answer.isEmpty()) {
-                        sb.append(answer.pollFirst());
+            if (c >= 'A' && c <= 'Z')
+                sb.append(c);
+            else {
+                // 괄호가 시작되면 무조건 push
+                if (c == '(') {
+                    s.push(c);
+                } else if (c == ')') {
+                    while (s.peek() != '(')
+                        sb.append(s.pop());
+                    s.pop();
+                } else {
+                    while (!s.isEmpty() && precedence(c) <= precedence(s.peek())) {
+                        sb.append(s.pop());
                     }
-                    check = false;
+                    s.push(c);
                 }
-            } else {
-                // 현재 stack에 push된것들 중에서
-                // ( 가 나올때까지 answer에 push
-                check = true;
-                add_to_value(s, answer);
-                s.pop();
             }
         }
 
-        add_to_value(s, answer);
-
-        while (!answer.isEmpty()) {
-            sb.append(answer.pollFirst());
+        while (!s.isEmpty()) {
+            sb.append(s.pop());
         }
 
         System.out.println(sb.toString());
         in.close();
-    }
-
-    static void add_to_value(Deque<Character> s, Deque<Character> answer) {
-        while (!s.isEmpty() && s.peek() != '(') {
-            char c_now = s.pop();
-            if (c_now >= 65 && c_now <= 90)
-                answer.addFirst(c_now);
-            else
-                answer.addLast(c_now);
-        }
     }
 }
